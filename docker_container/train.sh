6ccd52da7e5c6c5e7f8abacdc5aa48f7065ca07c
Carlos González <ce.gonzalez13@uniandes.edu.co>
2018-10-22 12:46:55 -0500

    added txt file with classes

--- docker_container/train.sh
+++ docker_container/train.sh
@@ -4,4 +4,5 @@ SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 docker run -v $SCRIPTPATH/tf_files:/tf_files -i -t cegonzalv/tactilefindertensorflow:latest bash -c "./train.sh $1 $2"
 cp -r ./tf_files/web_model/$1/  ../tactileFinderClient/src/tfmodel
+cp -r ./tf_files/retrained_labels.txt ../tactileFinderClient/src/tfmodel/$1/
 cmd /k
